788930d97590debe7738a6429be22347889d992e
Adrieli <58448769+adrielidaltoe@users.noreply.github.com>
2022-09-21 12:23:23 -0300

    Update linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -4,61 +4,82 @@ import numpy as np
 
 class LinearRegressionOLS:
     
-    '''This class fits a linear regression using Normal Equation (ordinary least squares).
-    Normalize the design matrix.'''
+    """
+    This class fits a linear regression using Normal Equation (ordinary least squares).
+    The design matrix must be normalized in order to work.
+    """
     
-    def reshape_x(self,X):
-        # verify if X object is an array numpy. It can be a pandas object too.
+    def reshape_x(self, X):
+        """
+        Verifies if X object is a numpy array. It can be a pandas object too.
+        """
         if not isinstance(X, np.ndarray):
             X = X.values
             
         # Reshaping the design matrix, if necessary
         if len(X.shape) == 1:
-            Xn = np.reshape(X, (X.shape[0],1))
+            Xn = np.reshape(X, (X.shape[0], 1))
         else:
             Xn = X.copy()
             
         # adding a new column of ones in Xn to calculate the intercept
         m = Xn.shape[0]
         Xn = np.insert(Xn, 0, np.ones(m), axis = 1)
+        
         return Xn
     
     def reshape_y(self, y):
-        # verifying if y is an array numpy
+        """
+        Verifies if y is a numpy array.
+        """
         if not isinstance(y, np.ndarray):
             y = y.values
+            
         y = np.reshape(y, (y.shape[0],1))
+        
         return y
     
-    def fit(self, X, y):
-                
-        # Reshaping the design matrix
+    def fit(self, X, y):      
+        """
+        Fits the model.
+        """
+        # Reshapes X
         Xn = self.reshape_x(X)
         
-        # Reshaping y
+        # Reshapes y
         y = self.reshape_y(y)
         
         # Fit with Normal Equation
-        inverse_matrix = np.linalg.inv(np.dot(Xn.T,Xn))
-        self.coef = np.dot(np.dot(inverse_matrix,Xn.T), y)
+        inverse_matrix = np.linalg.inv(np.dot(Xn.T, Xn))
+        
+        self.coef = np.dot(np.dot(inverse_matrix, Xn.T), y)
             
     def predict(self, X):
-        
-        # Reshaping the design matrix, if necessary
+        """
+        Makes predictions.
+        """
+        # Reshaping the design matrix
         Xn = self.reshape_x(X)
-        y_pred = np.dot(Xn,self.coef)
+        
+        y_pred = np.dot(Xn, self.coef)
+        
         return y_pred.flatten()
     
     def score(self, y_pred, y_true):
+        """
+        Estimates the score.
+        """
         return np.sum(((y_true - y_pred)**2))/y_true.shape[0]
 
 
 class LinearRegressionGD(LinearRegressionOLS):
     
-    '''This class fits a linear regression using gradient descent as the optimization algorithm.
-    Normalize the design matrix.'''
+    """
+    This class fits a linear regression using gradient descent as the optimization algorithm.
+    The design matrix must be normalized in order to work.
+    """
     
-    def __init__(self, iterations = 1000, alpha = 0.01, tolerance = 0.00001):
+    def __init__(self, alpha = 0.01, iterations = 1000, tolerance = 0.00001):
         self.alpha = alpha
         self.iterations = iterations
         self.tol = tolerance
@@ -73,7 +94,7 @@ class LinearRegressionGD(LinearRegressionOLS):
         
         # initializing the parameters. Xn is now a matrix [m x n+1]
         n = Xn.shape[1]
-        self.coef = np.random.randn(n,1)
+        self.coef = np.random.randn(n, 1)
             
         # Size of training set
         m = Xn.shape[0]
